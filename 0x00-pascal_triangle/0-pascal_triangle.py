4cf0b2e190b3fede23f0fbaba8b7a60239e93d82
weaver010 <abdelrahmannagaty839@gmail.com>
2024-05-30 17:06:07 +0000

    hi

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+
+
 def pascal_triangle(n):
-    result=[]
- 
-    for i in range (n):
-        num= 11 ** i
-        list_d=list(str(num))
-        result.append (list_d)
+    result = []
+    for i in range(n):
+        num = 11 ** i
+        list_d = list(str(num))
+        result.append(list_d)
     return result
